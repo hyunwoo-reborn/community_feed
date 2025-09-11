a9860a6e4dbbd38d5ba49d595d30b9d097b9ddcc
hyunwoo.park.reborn <hyunwoo.park.reborn@gmail.com>
2025-09-11 17:52:22 +0900

    인수테스트 템플릿 작성

---
+++ src/test/java/org/sayscampus/acceptance/utils/AcceptanceTestTemplate.java
@@ -0,0 +1,23 @@
+package org.sayscampus.acceptance.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AcceptanceTestTemplate {
+
+    @Autowired
+    private DatabaseCleanup cleanup;
+
+    @Autowired
+    private DataLoader loader;
+
+    @BeforeEach
+    public void init() {
+        cleanup.execute();
+        loader.loadData();
+    }
+}
---
+++ src/test/java/org/sayscampus/acceptance/utils/DataLoader.java
@@ -0,0 +1,23 @@
+package org.sayscampus.acceptance.utils;
+
+import org.sayscampus.user.application.dto.CreateUserRequestDto;
+import org.sayscampus.user.application.dto.FollowUserRequestDto;
+import org.springframework.stereotype.Component;
+
+import static org.sayscampus.acceptance.steps.UserAcceptanceSteps.createUser;
+import static org.sayscampus.acceptance.steps.UserAcceptanceSteps.followUser;
+
+@Component
+public class DataLoader {
+
+    public void loadData() {
+        // user 1, 2, 3 생성 및 user 1 follow user 2, user 3
+        CreateUserRequestDto user = new CreateUserRequestDto("test user", "");
+        createUser(user);
+        createUser(user);
+        createUser(user);
+
+        followUser(new FollowUserRequestDto(1L, 2L));
+        followUser(new FollowUserRequestDto(1L, 3L));
+    }
+}
